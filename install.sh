b52ffd9b80fc271e7a9593b5b707a1a8cc671d5f
Chirath R <chirath.02@gmail.com>
2018-05-03 21:47:18 +0530

    Install script

--- install.sh
+++ install.sh
@@ -2,18 +2,19 @@
 
 # install pip and wifi
 sudo apt install python3-pip
-sudo -H pip3 install wifi
+sudo -H pip3 install request
+sudo -H pip3 install requests
 
 # Store configuration files
 mkdir ~/.attendance
 cp ./attendance/* -r ~/.attendance/
-chmod +x ~/.attendance/config ~/.attendance/attendance.py ~/.attendance/get_interface_name.sh
+chmod +x ~/.attendance/config ~/.attendance/attendance.py ~/.attendance/get_interface_name.sh ~/.attendance/get_ssid_names.sh
 
 # Add a new cron-job
 # write out current crontab
-crontab -l > mycron
+sudo crontab -l > mycron
 # echo new cron into cron file, run every 15 mins
 echo "*/15 * * * * ~/.attendance/config" >> mycron
 # install new cron file
-crontab mycron
+sudo crontab mycron
 rm mycron
