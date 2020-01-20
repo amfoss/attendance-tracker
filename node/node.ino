533c4400cb228085b36e01aea0f527b9110b1e6a
chromicle <ajayprabhakar369@gmail.com>
2020-01-20 17:37:34 +0530

    node: Save seed value in EEPROM for every 5 mins

--- node/node.ino
+++ node/node.ino
@@ -3,21 +3,36 @@
 #include <ESP8266WebServer.h>
 #include <time.h>
 #include <Ticker.h>
+#include <EEPROM.h>
 
 Ticker blinker;
 
 const char* password = "";
 char ssid[100];
+unsigned int addr = 0;
 unsigned long long a, c, m, seed;
-void change_seed(){
-  seed = (seed%m) * (a%m);
-  seed = seed%m;
+
+void change_seed() {
+  seed = (seed % m) * (a % m);
+  seed = seed % m;
   seed += c;
   seed %= m;
 }
 
-void changeWifi(){
+void get_seed() {
+  EEPROM.get(addr, seed);
+  Serial.print("current seed changed in EEPROM");
+  if (seed == NULL){
+    seed = 1000;
+    EEPROM.put(addr, seed);
+    EEPROM.commit();
+  }
+}
+
+void changeWifi() {
   change_seed();
+  EEPROM.put(addr, seed);
+  EEPROM.commit();
   sprintf(ssid, "amFOSS_%d", seed);
   WiFi.softAP(ssid, password);
   Serial.println(ssid);
@@ -26,7 +41,9 @@ void changeWifi(){
 void setup() {
   delay(1000);
   Serial.begin(115200);
+  EEPROM.begin(512);
   Serial.print("Initial Setup");
+  get_seed();
   change_seed();
   sprintf(ssid, "amFOSS_%d", seed);
   WiFi.softAP(ssid, password);
